e429eaa1098036c9b34990262a04996f4b494db9
Tidjini <tidjini.messaoudi@gmail.com>
2022-11-30 05:18:48 +0100

    Add user Model

---
+++ apps/chats/models.py
@@ -0,0 +1,51 @@
+import uuid
+
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.postgres.fields import ArrayField
+
+
+class TimeStampedModel(models.Model):
+    date_creation = models.DateTimeField(auto_now_add=True)
+    date_modification = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Utilisateur(AbstractBaseUser, TimeStampedModel):
+    # ovveride primary key with char key
+    id = models.CharField(max_length=100, primary_key=True, default=uuid.uuid4)
+    username = models.CharField(max_length=30, unique=True)
+    nom = models.CharField(max_length=30, null=False, blank=False)
+    prenom = models.CharField(max_length=30, null=False, blank=False)
+    sex = models.IntegerField(default=1)
+
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    photo = models.ImageField(null=True, max_length=1024)
+    telephone = models.CharField(max_length=20, null=True, blank=True)
+    telephone_deux = models.CharField(max_length=20, null=True, blank=True)
+    # as react with fuse
+    role = ArrayField(
+        models.CharField(max_length=50, blank=True, null=True), blank=True, default=list
+    )
+
+    date_naissance = models.DateField(null=True, blank=True)
+
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["nom", "prenom"]
+
+    # travail sur une base de données spécifier
+    database_name = models.CharField(max_length=50, null=True, blank=True)
+    # work on exercice by year 2021 (id)
+    exercice = models.CharField(max_length=100, null=True, blank=True)
+    # authorize to work on
+    database_authorized = ArrayField(
+        models.CharField(max_length=50, blank=True, null=True), blank=True, default=list
+    )
+
+    def __str__(self):
+        return self.username
--- readme.rst
+++ readme.rst
@@ -6,3 +6,5 @@ For message testing application, using: python socketio with gevent library
 Each user can create a discusion room
 Each discussion room contain many owners / many visitors
 Each discusion contain messages with sender / reciever
+
+1/ clean users AUTH
