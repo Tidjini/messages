bf5b8cf403608cc91e992ac9672a88fdbd0ddeb5
Tidjini <tidjini.messaoudi@gmail.com>
2022-12-04 09:27:58 +0100

    Set Allowed hosts

--- config/wsgi.py
+++ config/wsgi.py
@@ -13,9 +13,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 django_app = get_wsgi_application()
 application = socketio.WSGIApp(sio, django_app)
 
-server = pywsgi.WSGIServer(('', 8000), application, handler_class=WSGIHandler)
+server = pywsgi.WSGIServer(
+    ('0.0.0.0', 8000), application, handler_class=WSGIHandler)
 server.serve_forever()
-
-
-
-
