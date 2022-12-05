f330decc91ee137b4ace65b76c4cead9fe9896d1
Tidjini <tidjini.messaoudi@gmail.com>
2022-12-05 13:58:27 +0100

    set default page size to 200

--- config/settings/base.py
+++ config/settings/base.py
@@ -146,7 +146,7 @@ REST_FRAMEWORK = {
     ),
     "DEFAULT_FILTER_BACKENDS": ("django_filters.rest_framework.DjangoFilterBackend",),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 50,
+    "PAGE_SIZE": 200,
     "COERCE_DECIMAL_TO_STRING": False,
     "COERCE_DATE_TO_STRING": False,
 }
