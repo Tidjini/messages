54451641e92cf792884a2ffb464f0efd9457d089
Tidjini <tidjini.messaoudi@gmail.com>
2022-12-07 12:38:55 +0100

    set gevent running

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,19 +1,23 @@
 """"""
 import os
-# import socketio
-# from geventwebsocket import WebSocketServer
+import socketio
+from geventwebsocket import WebSocketServer
 
 # django
 from django.core.wsgi import get_wsgi_application
-# from apps.communications.views import sio
+
+# app
+from apps.communications.views import sio
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-application = get_wsgi_application()
-app = application
-# application = socketio.WSGIApp(sio, django_app)
+web_app = get_wsgi_application()
+# app instead of application for vercel
+app = socketio.WSGIApp(sio, web_app)
 
-# server = WebSocketServer(
-#     # ('0.0.0.0', 8000), application, handler_class=WSGIHandler)
-#     ('0.0.0.0', 8000), application)
-# server.serve_forever()
+PORT = os.environ.get('PORT', 8000)
+
+server = WebSocketServer(
+    # ('0.0.0.0', 8000), application, handler_class=WSGIHandler)
+    ('0.0.0.0', PORT), app)
+server.serve_forever()
