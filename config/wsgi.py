1d2c00a34ba80086cba5e68d849d3421866105cc
Tidjini <tidjini.messaoudi@gmail.com>
2022-12-07 13:57:00 +0100

    check with 8000

--- config/wsgi.py
+++ config/wsgi.py
@@ -11,16 +11,16 @@ from apps.communications.views import sio
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-app = get_wsgi_application()
+web_app = get_wsgi_application()
 # app instead of application for vercel
-# app = socketio.WSGIApp(sio, web_app)
+app = socketio.WSGIApp(sio, web_app)
 
 
-# server = WebSocketServer(
-#     # ('0.0.0.0', 8000), application, handler_class=WSGIHandler)
-#     ('0.0.0.0', 8000), app)
+server = WebSocketServer(
+    # ('0.0.0.0', 8000), application, handler_class=WSGIHandler)
+    ('0.0.0.0', 8000), app)
 
 # server = WebSocketServer(
 #     # ('0.0.0.0', 8000), application, handler_class=WSGIHandler)
 #     ('0.0.0.0', int(PORT)), app)
-# server.serve_forever()
+server.serve_forever()
