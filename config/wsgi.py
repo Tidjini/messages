9695443c978d4029b4cbe65d0807965a02324144
Tidjini <tidjini.messaoudi@gmail.com>
2022-12-07 12:51:08 +0100

    set port

--- config/wsgi.py
+++ config/wsgi.py
@@ -15,9 +15,9 @@ web_app = get_wsgi_application()
 # app instead of application for vercel
 app = socketio.WSGIApp(sio, web_app)
 
-PORT = os.environ.get('PORT', 8000)
+PORT = os.environ.get('PORT', '8000')
 
 server = WebSocketServer(
     # ('0.0.0.0', 8000), application, handler_class=WSGIHandler)
-    ('0.0.0.0', PORT), app)
+    ('0.0.0.0', int(PORT)), app)
 server.serve_forever()
