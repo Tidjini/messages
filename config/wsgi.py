27436e5f417ccde1fcc6fc7ab49875add4fb7889
Tidjini <tidjini.messaoudi@gmail.com>
2022-12-07 10:52:54 +0100

    test with server application no sockets

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,18 +1,19 @@
 """"""
 import os
-import socketio
-from geventwebsocket import WebSocketServer
+# import socketio
+# from geventwebsocket import WebSocketServer
 
 # django
 from django.core.wsgi import get_wsgi_application
-from apps.communications.views import sio
+# from apps.communications.views import sio
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-django_app = get_wsgi_application()
-application = socketio.WSGIApp(sio, django_app)
+application = get_wsgi_application()
+app = application
+# application = socketio.WSGIApp(sio, django_app)
 
-server = WebSocketServer(
-    # ('0.0.0.0', 8000), application, handler_class=WSGIHandler)
-    ('0.0.0.0', 8000), application)
-server.serve_forever()
+# server = WebSocketServer(
+#     # ('0.0.0.0', 8000), application, handler_class=WSGIHandler)
+#     ('0.0.0.0', 8000), application)
+# server.serve_forever()
